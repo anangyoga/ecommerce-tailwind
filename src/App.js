83fc7ba3b831fe345f11028a36b74b83f7e662fc
anangyoga <aharwinto@gmail.com>
2022-03-18 23:51:01 +0700

    feat: change the loop-card-element into a reusable component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Card from "./components/Card";
 import Menu from "./components/Menu";
 import products from "./services/data/product";
 
@@ -385,21 +386,7 @@ function App() {
           <div className="flex flex-wrap justify-center gap-8">
             <div className="flex gap-8  flex-wrap">
               {products.map((product, index) => {
-                const priceAfterDiscounted = product.originalPrice - (product.originalPrice * product.discountPercent) / 100;
-                return (
-                  <div className="border-2 border-gray-200 px-3 rounded-lg">
-                    <img src={product.image} alt="" className="mt-3" />
-                    <h2 className="font-bold mb-3 mt-3">{product.name}</h2>
-                    <p className="text-gray-500 text-xs">{product.desc}</p>
-                    <div className="flex justify-between mt-3">
-                      <div className="flex-col">
-                        <p className="font-bold">{priceAfterDiscounted}</p>
-                        {product.discountPercent > 0 && <p className="text-gray-500 text-xs line-through">{product.originalPrice}</p>}
-                      </div>
-                      <button className="h-8 px-2 font-bold text-white bg-green-600 rounded-lg">Buy Now</button>
-                    </div>
-                  </div>
-                );
+                return <Card product={product} key={index} />;
               })}
             </div>
           </div>
---
+++ src/components/Card.jsx
@@ -0,0 +1,23 @@
+const Card = ({ product }) => {
+  // {product} berasal dari:
+  // const product = props.product
+
+  const priceAfterDiscounted = product.originalPrice - (product.originalPrice * product.discountPercent) / 100;
+
+  return (
+    <div className="border-2 border-gray-200 px-3 rounded-lg">
+      <img src={product.image} alt="" className="mt-3" />
+      <h2 className="font-bold mb-3 mt-3">{product.name}</h2>
+      <p className="text-gray-500 text-xs">{product.desc}</p>
+      <div className="flex justify-between mt-3">
+        <div className="flex-col">
+          <p className="font-bold">Rp {priceAfterDiscounted}</p>
+          {product.discountPercent > 0 && <p className="text-gray-500 text-xs line-through">{product.originalPrice}</p>}
+        </div>
+        <button className="h-8 px-2 font-bold text-white bg-green-600 rounded-lg">Buy Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
